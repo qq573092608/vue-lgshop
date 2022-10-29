9c4c278dc7a9dee78c9bdb76564021f309dd7ac7
qq573092608 <1466081601@qq.com>
2022-10-30 01:14:58 +0800

    重點：keepalive的使用以及涉及嵌套路由的時候活躍狀態的保持

--- vue-shop/src/components/Product.vue
+++ vue-shop/src/components/Product.vue
@@ -28,7 +28,22 @@ export default {
   watch: {},
   created () {},
   mounted () {},
-  methods: {}
+  methods: {},
+  // 記錄上一次離開的位置
+  beforeRouteLeave(to, from, next){
+    console.log('beforeRouteLeave===' + this.$route.path)
+    this.path = this.$route.path
+    next()
+  },
+  // 活躍的時候主動跳轉到上一次記錄的位置
+  activated(){
+    this.$router.push(this.path)
+  },
+  // **************activated、deactivated 這兩個函數，只有該組件被保持了狀態使用了keep-alive的時候才有效：切記切記*************
+  // 因爲離開了keep-alive函數那組件就被銷毀了，自然這兩個函數就沒有用了
+  deactivated(){
+    
+  }
 }
 </script>
 
