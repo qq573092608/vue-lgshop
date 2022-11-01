86bfab37ec4188f5a5bcb4a39ad2304ef73c7db7
qq573092608 <1466081601@qq.com>
2022-11-02 01:14:56 +0800

    配置目录结构，并且设置normalize.scss

--- vue-shop/src/main.js
+++ vue-shop/src/main.js
@@ -18,33 +18,3 @@ new Vue({
 }).$mount('#app')
 
 
-axios.defaults.baseURL = 'http://123.207.32.32:8000'
-axios.defaults.timeout = 5000
-
-axios({
-  url:'/home/multidata'
-}).then(res => {
-  console.log(res)
-})
-
-axios.all(
-  [
-    axios(
-      {
-        url:'/home/multidata'
-      }
-    ),
-    axios(
-      {
-        url:'/home/data',
-        params: {
-          type: 'sell',
-          page: 4
-        }
-      }
-    )
-  ]
-).then(res => {
-    console.log(res)
-})
-
